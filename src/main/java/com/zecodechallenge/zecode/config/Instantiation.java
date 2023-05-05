5a0425786c53a65a73f3ae7eeb68188960f94683
Matheus <matheus.mnascimento@tivit.com>
2023-05-05 18:55:20 -0300

    add app prod

--- src/main/java/com/zecodechallenge/zecode/config/Instantiation.java
+++ src/main/java/com/zecodechallenge/zecode/config/Instantiation.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
